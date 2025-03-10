120a2d49a7b5a9f6b09c46d7e80b190e73ce0cda
bhavin3206 <bhavinpatel99987@gmail.com>
2025-03-10 15:22:04 +0530

    add setup config

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="selenium_helper",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "selenium"
+    ],
+    author="Bhavin Patel",
+    author_email="bhavinpatel99987@gmail.com",
+    description="A helper utility for Selenium WebDriver automation.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bhavin3206/selenium_helper",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
